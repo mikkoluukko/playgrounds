d3a310a53b02cb70f22eedfe6a33c8a2f876ca73
Mikko Luukko <mikkoluukko@hotmail.com>
2021-01-17 10:26:09 +0200

    update for Heroku deployment

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const playgroundRoutes = require('./routes/playgrounds');
 const reviewRoutes = require('./routes/reviews');
-// const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://localhost:27017/playgrounds-on-map';
+const dbUrl =
+    process.env.DB_URL || 'mongodb://localhost:27017/playgrounds-on-map';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
